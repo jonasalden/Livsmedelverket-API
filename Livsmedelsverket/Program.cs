b912033402ce1a2ebbb3335f41a5d1fbdc1f0414
Jonas Aldén <jonas.alden@dormy.se>
2020-11-16 23:51:01 +0100

    Option to create directory, Override existing file or save as new file, Method to simplify logging.

--- Livsmedelsverket/Program.cs
+++ Livsmedelsverket/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RestSharp;
 using System;
+using System.IO;
 using System.Xml;
 
 namespace Livsmedelsverket
@@ -14,15 +15,16 @@ namespace Livsmedelsverket
         #region Methods
         static void Main()
         {
-            Console.WriteLine("Livsmedelsverket API");
-            Console.WriteLine("Välj data att hämta: (1) Näringsvärde, (2) Klassificering");
+            DoLog("Livsmedelsverket API");
+            DoLog("Select data to download: (1) Näringsvärde / (2) Klassificering");
 
-            string data = Console.ReadLine();
-            bool success = int.TryParse(data, out int category);
+            string selectedCategory = Console.ReadLine();
+            bool success = int.TryParse(selectedCategory, out int category);
 
             if (success)
             {
-                Console.WriteLine("Vilket datum vill du hämta data ifrån? Ange yyyy-MM-dd");
+                DoLog("Specify date for data: yyyy-MM-dd");
+
                 string dateval = Console.ReadLine();
 
                 var dateSuccess = DateTime.TryParse(dateval, out DateTime date);
@@ -43,7 +45,7 @@ namespace Livsmedelsverket
 
                     var client = new RestClient(_apiUrl);
                     var request = new RestRequest($"{type}/{date:yyyy-MM-dd}", DataFormat.Xml);
-                    Console.WriteLine($"Trying to GET {type} data from {date:yyyy-MM-dd}");
+                    DoLog($"Trying to GET {type} data from {date:yyyy-MM-dd}");
 
                     try
                     {
@@ -51,42 +53,91 @@ namespace Livsmedelsverket
 
                         if (response.IsSuccessful)
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("GET Data succeded.");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine("Preparing XML.");
+                            DoLog("GET Data succeded.", ConsoleColor.Green);
+                            DoLog("Preparing XML.");
 
                             var xmlDoc = new XmlDocument();
                             xmlDoc.LoadXml(response.Content);
-                            xmlDoc.Save($"{_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml");
 
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Success! Saved file to {_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml");
+                            bool directoryExist = Directory.Exists($"{_rootPath}\\Livsmedelsverket");
+
+                            if (!directoryExist)
+                            {
+                                DoLog($"Directory {_rootPath}\\Livsmedelsverket not found.", ConsoleColor.Yellow);
+                                DoLog("Do you want to create folder for the retrieved data? Y/N", ConsoleColor.Yellow);
+
+                                var answer = Console.ReadLine();
+
+                                if (!string.IsNullOrWhiteSpace(answer))
+                                {
+                                    answer = answer.ToLower().Trim();
+
+                                    if (answer == "y")
+                                    {
+                                        Directory.CreateDirectory($"{_rootPath}\\Livsmedelsverket");
+                                        DoLog($"Directory {_rootPath}\\Livsmedelsverket created.", ConsoleColor.Green);
+                                    }
+                                    else if (answer == "n")
+                                    {
+                                        DoLog($"Directory {_rootPath}\\Livsmedelsverket not created.", ConsoleColor.Red);
+                                    }
+                                    else
+                                    {
+                                        DoLog("Not a legimate answer.", ConsoleColor.Red);
+                                    }
+                                }
+                            }
+
+                            if (File.Exists($"{_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml"))
+                            {
+                                DoLog("File exists, Do you want to override it? Y/N");
+                                var answer = Console.ReadLine();
+                                answer = answer.ToLower().Trim();
+                                DoLog("--------------", ConsoleColor.Green);
+
+                                if (answer == "y")
+                                {
+                                    xmlDoc.Save($"{_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml");
+                                    DoLog($"Success! Overrided {_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml", ConsoleColor.Green);
+                                }
+                                else if (answer == "n")
+                                {
+                                    xmlDoc.Save($"{_rootPath}\\Livsmedelsverket\\{type.ToLower()}_{DateTime.Now.ToString("yyyyMMddHHmmssFFF")}.xml");
+                                    DoLog($"Success! Created {_rootPath}\\Livsmedelsverket\\{type.ToLower()}_{DateTime.Now.ToString("yyyyMMddHHmmssFFF")}.xml", ConsoleColor.Green);
+                                }
+                            }
+                            else
+                            {
+                                xmlDoc.Save($"{_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml");
+                                DoLog($"Success! Created {_rootPath}\\Livsmedelsverket\\{type.ToLower()}.xml", ConsoleColor.Green);
+                            }
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Getting Data failed");
+                            DoLog("Getting Data failed", ConsoleColor.Red);
                         }
 
                     }
                     catch (Exception e)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Getting Data failed, Error: " + e.InnerException);
+                        DoLog("Getting data failed, Error: " + e.Message, ConsoleColor.Red);
                     }
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Incorrect Date input: '{dateval}' yyyy-MM-dd");
+                    DoLog($"Incorrect Date input: '{dateval}' yyyy-MM-dd", ConsoleColor.Red);
                 }
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Incorrect Category input: '{data}' should be 1 or 2.");
+                DoLog($"Incorrect Category input: '{selectedCategory}, should be 1 or 2.'", ConsoleColor.Red);
             }
+        }
+
+        public static void DoLog(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
             Console.ResetColor();
         }
         #endregion
